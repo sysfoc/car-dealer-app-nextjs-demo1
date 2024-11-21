0358fb4aa857a05b81442880f0166c313cd2478b
Hamza-fullstackdev <hamzafullstackdev1@gmail.com>
2024-11-21 13:45:48 +0500

    Install New Fonts For The App

--- app/components/VehicalsList.jsx
+++ app/components/VehicalsList.jsx
@@ -7,68 +7,67 @@ import { GiGasPump } from "react-icons/gi";
 import { TbManualGearbox } from "react-icons/tb";
 
 const VehicalsList = () => {
-    const vehicals= [
-        {
-            name: "Audi A6 3.5",
-            image: "/Luxury SUV.jpg",
-            description: "3.5 D5 PowerPulse Momentum 5dr AWD Geartronic Estate",
-            price: 25000,
-            fuelType: "Petrol",
-            driven: 50,
-            transmission: "Automatic",
-        },
-        {
-            name: "Audi A6 3.5",
-            image: "/Luxury SUV.jpg",
-            description: "3.5 D5 PowerPulse Momentum 5dr AWD Geartronic Estate",
-            price: 25000,
-            fuelType: "Petrol",
-            driven: 25,
-            transmission: "Automatic",
-        },
-        {
-            name: "Audi A6 3.5",
-            image: "/Luxury SUV.jpg",
-            description: "3.5 D5 PowerPulse Momentum 5dr AWD Geartronic Estate",
-            price: 25000,
-            fuelType: "Petrol",
-            driven: 30,
-            transmission: "Automatic",
-        },
-        {
-            name: "Audi A6 3.5",
-            image: "/Luxury SUV.jpg",
-            description: "3.5 D5 PowerPulse Momentum 5dr AWD Geartronic Estate",
-            price: 25000,
-            fuelType: "Petrol",
-            driven: 60,
-            transmission: "Automatic",
-        },
-        {
-            name: "Audi A6 3.5",
-            image: "/Luxury SUV.jpg",
-            description: "3.5 D5 PowerPulse Momentum 5dr AWD Geartronic Estate",
-            price: 25000,
-            fuelType: "Petrol",
-            driven: 70,
-            transmission: "Automatic",
-        },
-    ]
+  const vehicals = [
+    {
+      name: "Audi A6 3.5",
+      image: "/Luxury SUV.jpg",
+      description: "3.5 D5 PowerPulse Momentum 5dr AWD Geartronic Estate",
+      price: 25000,
+      fuelType: "Petrol",
+      driven: 50,
+      transmission: "Automatic",
+    },
+    {
+      name: "Audi A6 3.5",
+      image: "/Luxury SUV.jpg",
+      description: "3.5 D5 PowerPulse Momentum 5dr AWD Geartronic Estate",
+      price: 25000,
+      fuelType: "Petrol",
+      driven: 25,
+      transmission: "Automatic",
+    },
+    {
+      name: "Audi A6 3.5",
+      image: "/Luxury SUV.jpg",
+      description: "3.5 D5 PowerPulse Momentum 5dr AWD Geartronic Estate",
+      price: 25000,
+      fuelType: "Petrol",
+      driven: 30,
+      transmission: "Automatic",
+    },
+    {
+      name: "Audi A6 3.5",
+      image: "/Luxury SUV.jpg",
+      description: "3.5 D5 PowerPulse Momentum 5dr AWD Geartronic Estate",
+      price: 25000,
+      fuelType: "Petrol",
+      driven: 60,
+      transmission: "Automatic",
+    },
+    {
+      name: "Audi A6 3.5",
+      image: "/Luxury SUV.jpg",
+      description: "3.5 D5 PowerPulse Momentum 5dr AWD Geartronic Estate",
+      price: 25000,
+      fuelType: "Petrol",
+      driven: 70,
+      transmission: "Automatic",
+    },
+  ];
   return (
     <section className="mx-4 my-10 sm:mx-12 md:my-20">
       <div className="flex items-center justify-between">
         <h2 className="text-3xl font-semibold">Explore All Vehicles</h2>
         <Link href={"/"}>
-          <p className="inline-flex items-center gap-x-3 text-lg">
+          <p className="text-md inline-flex items-center gap-x-3">
             View All <MdOutlineArrowOutward />
           </p>
         </Link>
       </div>
       <div className="mt-3 border-b-2 border-gray-300"></div>
       <div className="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-       {
-        vehicals.map((vehical,index)=>(
-            <div className="shadow-md rounded-xl overflow-hidden" key={index}>
+        {vehicals.map((vehical, index) => (
+          <div className="overflow-hidden rounded-xl shadow-md" key={index}>
             <div>
               <Image
                 src={`${vehical.image}`}
@@ -79,25 +78,27 @@ const VehicalsList = () => {
               />
             </div>
             <div className="my-3 px-4">
-              <h3 className="font-semibold text-xl">{vehical.name}</h3>
-              <p className="text-md">{vehical.description.slice(0,27)}...</p>
+              <h3 className="text-xl font-semibold">{vehical.name}</h3>
+              <p className="text-md text-sm">
+                {vehical.description.slice(0, 26)}...
+              </p>
               <div className="mt-3 border-b-2 border-gray-100"></div>
               <div className="my-3 grid grid-cols-3 gap-3">
                 <div className="text-center">
                   <div className="flex items-center justify-center">
-                   <IoSpeedometer fontSize={25}/>
+                    <IoSpeedometer fontSize={25} />
                   </div>
                   <p className="mt-2 text-sm">{vehical.driven} Miles</p>
                 </div>
                 <div className="text-center">
                   <div className="flex items-center justify-center">
-                    <GiGasPump fontSize={25}/>
+                    <GiGasPump fontSize={25} />
                   </div>
                   <p className="mt-2 text-sm">{vehical.fuelType}</p>
                 </div>
                 <div className="text-center">
                   <div className="flex items-center justify-center">
-                   <TbManualGearbox fontSize={25}/>
+                    <TbManualGearbox fontSize={25} />
                   </div>
                   <p className="mt-2 text-sm">{vehical.transmission}</p>
                 </div>
@@ -105,10 +106,13 @@ const VehicalsList = () => {
               <div className="mt-3 border-b-2 border-gray-100"></div>
               <div className="my-3 flex items-center justify-between">
                 <div>
-                  <h4 className="text-xl font-semibold">${vehical.price}</h4>
+                  <h4 className="text-lg font-semibold">${vehical.price}</h4>
                 </div>
                 <div>
-                  <Link href={"/"} className="font-semibold text-blue-950 dark:text-red-500">
+                  <Link
+                    href={"/"}
+                    className="font-semibold text-blue-950 dark:text-red-500"
+                  >
                     <p className="inline-flex items-center gap-x-3">
                       View Details <MdOutlineArrowOutward />
                     </p>
@@ -117,8 +121,7 @@ const VehicalsList = () => {
               </div>
             </div>
           </div>
-        ))
-       }
+        ))}
       </div>
     </section>
   );
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,15 @@
 import type { Metadata } from "next";
+import { Poppins } from '@next/font/google';
 import { ThemeModeScript } from "flowbite-react";
 import Navbar from "./components/Header";
 import Footer from "./components/Footerr"
 import "./globals.css";
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  style: ['normal', 'italic'],
+});
 
 export const metadata: Metadata = {
   title: "Auto Car Dealers",
@@ -19,7 +26,7 @@ export default function RootLayout({
       <head>
         <ThemeModeScript />
       </head>
-      <body className="dark:bg-gray-800 dark:text-gray-200">
+      <body className={`dark:bg-gray-800 dark:text-gray-200 ${poppins.className}`}>
         <Navbar/>
         {children}
         <Footer/>
