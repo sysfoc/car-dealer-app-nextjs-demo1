d9f7329f348bd00cd33eee32fdc00b7ad35aa82d
sysfoc <sysfoc@gmail.com>
2025-01-21 16:30:35 +0500

    connect debug

--- app/lib/mongodb.js
+++ app/lib/mongodb.js
@@ -1,30 +1,33 @@
-import { MongoClient } from "mongodb";
+// import { MongoClient } from "mongodb";
 
-const uri = process.env.MONGODB_URI;
-const options = {
-  useUnifiedTopology: true,
-  useNewUrlParser: true,
-};
-let client;
-``;
-let clientPromise;
+// const uri = process.env.MONGODB_URI;
+// const options = {
+//   useUnifiedTopology: true,
+//   useNewUrlParser: true,
+// };
+// let client;
+// ``;
+// let clientPromise;
 
-if (!process.env.MONGODB_URI) {
-  throw new Error("Add Mongo URI to .env.local");
-}
+// if (!process.env.MONGODB_URI) {
+//   throw new Error("Add Mongo URI to .env.local");
+// }
 
-if (process.env.NODE_ENV === "development") {
-  if (!global._mongoClientPromise) {
-    client = new MongoClient(uri, options);
-    global._mongoClientPromise = client.connect();
-  }
-  clientPromise = global._mongoClientPromise;
-} else {
-  client = new MongoClient(uri, options);
-  clientPromise = client.connect();
-}
+// if (process.env.NODE_ENV === "development") {
+//   if (!global._mongoClientPromise) {
+//     client = new MongoClient(uri, options);
+//     global._mongoClientPromise = client.connect();
+//   }
+//   clientPromise = global._mongoClientPromise;
+// } else {
+//   client = new MongoClient(uri, options);
+//   clientPromise = client.connect();
+// }
 
-export default clientPromise;
+// export default clientPromise;
+
+//2
+
 // import mongoose from "mongoose";
 
 // const connectDB = async () => {
@@ -44,3 +47,34 @@ export default clientPromise;
 // };
 
 // export default connectDB;
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    if (mongoose.connections[0].readyState) {
+      console.log("Already connected to MongoDB");
+      return;
+    }
+
+    const clientOptions = {
+      serverApi: {
+        version: "1",
+        strict: true,
+        deprecationErrors: true,
+      },
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    };
+
+    const uri = process.env.MONGODB_URI;
+    await mongoose.connect(uri, clientOptions);
+
+    await mongoose.connection.db.admin().command({ ping: 1 });
+    console.log("Pinged your deployment. Successfully connected to MongoDB!");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    throw new Error("Failed to connect to MongoDB");
+  }
+};
+
+export default connectDB;
