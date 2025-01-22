886498af37368d97e88c63c76ae4213b538e859c
Hamza-fullstackdev <hamzafullstackdev1@gmail.com>
2025-01-22 15:00:23 +0500

    Minor Fixes

--- app/components/Cookiebox.jsx
+++ app/components/Cookiebox.jsx
@@ -3,16 +3,16 @@ import React from "react";
 
 const Cookiebox = () => {
   return (
-    <section class="fixed bottom-5 right-5 z-10 shadow-lg">
-      <div class="w-[350px] rounded-md bg-white px-6 py-4 dark:bg-gray-700">
+    <section className="fixed bottom-5 right-5 z-10 shadow-lg">
+      <div className="w-[350px] rounded-md bg-white px-6 py-4 dark:bg-gray-700">
         <div>
           <div>
-            <h2 class="text-lg font-bold">We Use Cookies</h2>
-            <p class="mt-2">
+            <h2 className="text-lg font-bold">We Use Cookies</h2>
+            <p className="mt-2">
               We use cookies in order to enhance the overall user experience.
             </p>
           </div>
-          <div class="mt-5 flex flex-col gap-1">
+          <div className="mt-5 flex flex-col gap-1">
             <Button color={"dark"}>Only Essentials</Button>
             <Button color={"dark"}>Accept All</Button>
           </div>
