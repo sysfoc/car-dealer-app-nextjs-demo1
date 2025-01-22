ef7e503b894509e26bbe6f4d619b14971f797852
sysfoc <sysfoc@gmail.com>
2025-01-22 14:33:20 +0500

    api update

--- app/api/cars/route.js
+++ app/api/cars/route.js
@@ -8,8 +8,6 @@ const client = new MongoClient(uri);
 
 const uploadDir = path.join(process.cwd(), "public", "uploads");
 
-const defaultImageUrl = "/Luxury SUV.webp";
-
 if (!fs.existsSync(uploadDir)) {
   fs.mkdirSync(uploadDir, { recursive: true });
 }
@@ -45,75 +43,29 @@ export async function GET() {
     await client.close();
   }
 }
-//old
-// export async function POST(req) {
-//   try {
-//     const formData = await req.formData();
-
-//     const image = formData.get("image");
-
-//     if (!image) {
-//       return NextResponse.json(
-//         { error: "Image file is required" },
-//         { status: 400 },
-//       );
-//     }
-
-//     const fileName = image.name;
-//     const filePath = path.join(uploadDir, fileName);
-
-//     const buffer = Buffer.from(await image.arrayBuffer());
-//     await fs.promises.writeFile(filePath, buffer);
-
-//     const carData = {
-//       ...Object.fromEntries(formData.entries()),
-//       imageUrl: `/uploads/${fileName}`,
-//     };
-
-//     await client.connect();
-//     const db = client.db("cardealor");
-//     const result = await db.collection("cars").insertOne(carData);
-
-//     return NextResponse.json(
-//       { message: "Car added successfully", data: result },
-//       { status: 201 },
-//     );
-//   } catch (error) {
-//     return NextResponse.json(
-//       { error: "Failed to add car", details: error.message },
-//       { status: 500 },
-//     );
-//   } finally {
-//     await client.close();
-//   }
-// }
 
 export async function POST(req) {
   try {
     const formData = await req.formData();
+
     const image = formData.get("image");
 
-    let imageUrl = defaultImageUrl;
-
-    if (image) {
-      try {
-        const fileName = image.name;
-        const filePath = path.join(uploadDir, fileName);
-
-        const buffer = Buffer.from(await image.arrayBuffer());
-
-        if (process.env.NODE_ENV === "development") {
-          await fs.promises.writeFile(filePath, buffer);
-          imageUrl = `/uploads/${fileName}`;
-        }
-      } catch (err) {
-        console.error("Image upload failed. Using default image:", err.message);
-      }
+    if (!image) {
+      return NextResponse.json(
+        { error: "Image file is required" },
+        { status: 400 },
+      );
     }
+
+    const fileName = image.name;
+    const filePath = path.join(uploadDir, fileName);
+
+    const buffer = Buffer.from(await image.arrayBuffer());
+    await fs.promises.writeFile(filePath, buffer);
 
     const carData = {
       ...Object.fromEntries(formData.entries()),
-      imageUrl,
+      imageUrl: `/uploads/${fileName}`,
     };
 
     await client.connect();
